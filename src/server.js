c34e1bab28b4ed354ecd508eddb5c72a9b35e848
Allyson <allysoneduardo04@gmail.com>
2020-08-08 09:37:55 -0300

    enviando objeto proffy para study.html finalizado

--- src/server.js
+++ src/server.js
@@ -11,8 +11,8 @@ const proffys = [
     time_to: [1220]
 },
 { 
-    name: "Daniele Evangelista", 
-    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+    name: "Mayk Brito", 
+    avatar: "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4", 
     whatsapp: "9 98174567", 
     bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
     subject: "Química", 
@@ -28,11 +28,11 @@ function pageLanding(req, res) {
 }
 
 function pageStudy(req, res) {
-    return res.render("study.html")
+    return res.render("study.html", { proffys })
 }
 
 function pageGiveClasses(req, res) {
-    return res.render("give-classes.html", { proffys, title: "Hi from Nunjucks"})
+    return res.render("give-classes.html")
 }
 
 const express = require('express')
